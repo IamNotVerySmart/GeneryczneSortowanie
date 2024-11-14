f3e4e3f1946d6bad6abbe119c2091ace86d48a37
dalone <dalone@DESKTOP-1MLT416>
2024-11-14 10:38:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GeneryczneSortowanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 2, 1, 3, 7, 4, 2 ,0, 6, 9};
+            double[] doubleArray = { 5.7, 14.0, 6.9, 1.2, 42.0 };
+
+            Console.WriteLine("(intArray) = ");
+            Sortowanie.Wypisz(intArray);
+            Sortowanie.Sort(ref intArray);
+            Console.WriteLine("Posortowane: ");
+            Sortowanie.Wypisz(intArray);
+
+            Console.WriteLine("(doubleArray) = ");
+            Sortowanie.Wypisz(doubleArray);
+            Sortowanie.Sort(ref doubleArray);
+            Console.WriteLine("Posortowane: ");
+            Sortowanie.Wypisz(doubleArray);
+        }
+    }
+}
---
+++ Sortowanie.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneryczneSortowanie
+{
+    internal class Sortowanie
+    {
+        public static void Sort<T>(ref T[] tab) where T : IComparable<T>
+        {
+            for (int i = 0; i < tab.Length; i++)
+            {
+                T x = tab[i];
+                int j = i - 1;
+                while (j >= 0 && tab[j].CompareTo(x) > 0)
+                {
+                    tab[j + 1] = tab[j];
+                    j--;
+                }
+                tab[j + 1] = x;
+            }
+        }
+
+        private static int P<T>(ref T a, ref T b)
+        {
+            if (a is IComparable<T> comperable)
+            {
+                return comperable.CompareTo(b);
+            }
+            else if (a is IComparable comparable)
+            {
+                return comparable.CompareTo(b);
+            }
+            else
+            {
+                throw new ArgumentException("Typ nie implementuje IComparable.");
+            }
+        }
+
+        public static void Wypisz<T>(T[] tab)
+        {
+            foreach (var x in tab)
+            {
+                Console.Write(x + ", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
